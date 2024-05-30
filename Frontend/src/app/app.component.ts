77b78d4bcd2b0d4a9a7e03a2080ba31318b7ed56
Jordan Covilla <jordancovilla@hotmail.com>
2024-05-30 01:19:48 -0500

    footer added
    template for a simple footer added

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormularioRegistroComponent } from "./formulario-registro/formulario-registro.component";
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, FormularioRegistroComponent, NavBarComponent]
+    imports: [RouterOutlet, FormularioRegistroComponent, NavBarComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'domi';

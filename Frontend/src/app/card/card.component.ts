d14ddd20803334a62456b8f2f5e3c45f5e945abd
Jordan Covilla <jordancovilla@hotmail.com>
2024-06-08 12:50:11 -0500

    estilos agregados, card creada, API rest funcionando
    API funcionando con metodo GET, POST todavia faltante

--- Frontend/src/app/card/card.component.ts
+++ Frontend/src/app/card/card.component.ts
@@ -1,10 +1,12 @@
-import { Component, Input  } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
+import { APIrestService } from '../service/form-request.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [ RouterOutlet, RouterLink, RouterLinkActive,  ],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule ],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
@@ -12,6 +14,20 @@ export class CardComponent {
   @Input() data: any;
 
   ngOnInit(): void {
-    console.log('Datos recibidos en ComponenteB:', this.data);
+    // console.log('Datos recibidos en ComponenteB:', this.data);
   }
+
+constructor(private apiRestService: APIrestService) {}
+
+  ApiGetData(): void {
+    this.apiRestService.getData().subscribe(data => {
+      this.data = data;
+      // console.log("data recibida en card" + this.data);
+    },
+      error => {
+        console.error('Error al obtener datos:', error);
+      }
+    );
+  }
+
 }

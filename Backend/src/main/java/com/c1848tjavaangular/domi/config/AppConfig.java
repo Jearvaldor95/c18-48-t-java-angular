8618109faf2753d900c8a530ca57f55ac8556145
Jesús Valeta <jvaleta1995@gmail.com>
2024-06-09 09:11:17 -0500

    Agregando Bean para permitir las peticiones de origen cruzado

--- Backend/src/main/java/com/c1848tjavaangular/domi/config/AppConfig.java
+++ Backend/src/main/java/com/c1848tjavaangular/domi/config/AppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig {
@@ -19,5 +21,18 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebMvcConfigurer configurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                WebMvcConfigurer.super.addCorsMappings(registry);
+                registry.addMapping("/auth/**").allowedMethods("POST", "PUT", "GET", "DELETE", "OPTIONS");
+                registry.addMapping("/api/**").allowedMethods("POST", "PUT", "GET", "DELETE", "OPTIONS")
+                        .allowedOrigins("http://localhost:4200");
+            }
+        };
     }
 }
